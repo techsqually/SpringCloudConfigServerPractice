0874da9175a7182642c65125e4b07164f6eff66b
sanjaysaini <sanjaysaini@172.20.10.3>
2020-02-07 19:02:11 +0530

    commit cloud config server code

---
+++ src/test/java/com/techsquelly/pracitce/SpringCloudConfigPracticeApplicationTests.java
@@ -0,0 +1,10 @@
+package com.techsquelly.pracitce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigPracticeApplicationTests {
+
+	
+}
